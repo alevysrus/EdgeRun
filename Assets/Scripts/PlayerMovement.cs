9f3aac2cd1716140fc876bb66f65c97c14a3c1ee
Alexey Vysotin <38469482+alevysrus@users.noreply.github.com>
2022-04-11 13:46:45 +0300

    0.54.02
    Add fly

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -26,168 +26,188 @@ public class PlayerMovement : MonoBehaviour
     int indexOfDelayForLadder = 0;
     bool laddercheckcondition;
 
-    //float y;
+    private float y;
+    private bool isGod;
 
+    private void Start()
+    {
+        isGod = false;
+    }
 
     private void Update()
     {
-        laddercheckcondition = Physics.CheckSphere(ladderCheck.position, 1f, ladderMask);
-        Activators.isGrounded = Physics.CheckSphere(groundCheck.position, 0.4f, groundMask);
+        if (!isGod)
+        {
+            laddercheckcondition = Physics.CheckSphere(ladderCheck.position, 1f, ladderMask);
+            Activators.isGrounded = Physics.CheckSphere(groundCheck.position, 0.4f, groundMask);
 
-        if (isLaddered && !laddercheckcondition)
-        {
-            indexOfDelayForLadder++;
-        }
-
-        switch (indexOfDelayForLadder, laddercheckcondition)
-        {
-            case (0 , true):
-                isLaddered = true;
-                break;
-            case (1 | 2 | 3  , false | true):
-                isLaddered = true;
-                Activators.isGrounded = true;
-                break;
-            case(4 , false):
-                isLaddered = false;
-                indexOfDelayForLadder = 0;
-                break;
-            default:
-                break;
-        }
-
-        if ((Activators.isGrounded | isLaddered) && velocity.y < 0)
-        {
-            velocity.y = -2f;
-        }
-        if (Input.GetButton("Jump") && (Activators.isGrounded | isLaddered))
-        {
-            velocity.y = Mathf.Sqrt(jumpHeihgt * -2f * gravity);
-        }
-        else
-        {
-            velocity.z = 0f;
-        }
-
-        x = Input.GetAxis("Horizontal");
-        z = Input.GetAxis("Vertical");
-
-        if (x == xChanched & Mathf.Abs(x) != 1 & x != 0)
-        {
-            x = 0;
-        }
-        else
-        {
-            if (Mathf.Abs(x) == 1)
+            if (isLaddered && !laddercheckcondition)
             {
-                x = Input.GetAxisRaw("Horizontal");
+                indexOfDelayForLadder++;
+            }
+
+            switch (indexOfDelayForLadder, laddercheckcondition)
+            {
+                case (0, true):
+                    isLaddered = true;
+                    break;
+                case (1 | 2 | 3, false | true):
+                    isLaddered = true;
+                    Activators.isGrounded = true;
+                    break;
+                case (4, false):
+                    isLaddered = false;
+                    indexOfDelayForLadder = 0;
+                    break;
+                default:
+                    break;
+            }
+
+            if ((Activators.isGrounded | isLaddered) && velocity.y < 0)
+            {
+                velocity.y = -2f;
+            }
+            if (Input.GetButton("Jump") && (Activators.isGrounded | isLaddered))
+            {
+                velocity.y = Mathf.Sqrt(jumpHeihgt * -2f * gravity);
             }
             else
             {
-                x = Input.GetAxis("Horizontal");
-                xChanched = x;
+                velocity.z = 0f;
             }
 
-        }
-        if (z == zChanched & Mathf.Abs(z) != 1 & z != 0)
-        {
-            z = 0;
-        }
-        else
-        {
-            if (Mathf.Abs(z) == 1)
+            x = Input.GetAxis("Horizontal");
+            z = Input.GetAxis("Vertical");
+
+            if (x == xChanched & Mathf.Abs(x) != 1 & x != 0)
             {
-                z = Input.GetAxisRaw("Vertical");
+                x = 0;
             }
             else
             {
-                z = Input.GetAxis("Vertical");
-                zChanched = z;
+                if (Mathf.Abs(x) == 1)
+                {
+                    x = Input.GetAxisRaw("Horizontal");
+                }
+                else
+                {
+                    x = Input.GetAxis("Horizontal");
+                    xChanched = x;
+                }
+
             }
-        }
+            if (z == zChanched & Mathf.Abs(z) != 1 & z != 0)
+            {
+                z = 0;
+            }
+            else
+            {
+                if (Mathf.Abs(z) == 1)
+                {
+                    z = Input.GetAxisRaw("Vertical");
+                }
+                else
+                {
+                    z = Input.GetAxis("Vertical");
+                    zChanched = z;
+                }
+            }
 
-        Vector3 laddermoving = (transform.right * x + transform.up * z);
-        Vector3 moving = (transform.right * x + transform.forward * z);
+            Vector3 laddermoving = (transform.right * x + transform.up * z);
+            Vector3 moving = (transform.right * x + transform.forward * z);
 
-        switch ((isLaddered, Activators.isGrounded))
-        {
-            case (true, true):
-                controller.Move(gamevelocity * Time.deltaTime * moving);
-                break;
-            case (true, false):
-                controller.Move(gamevelocity * Time.deltaTime * laddermoving);
-                break;
-            case (false, true):
-                controller.Move(gamevelocity * Time.deltaTime * moving);
-                break;
-            case (false, false):
-                controller.Move(gamevelocity * Time.deltaTime * moving);
-                break;
-        }
+            switch ((isLaddered, Activators.isGrounded))
+            {
+                case (true, true):
+                    controller.Move(gamevelocity * Time.deltaTime * moving);
+                    break;
+                case (true, false):
+                    controller.Move(gamevelocity * Time.deltaTime * laddermoving);
+                    break;
+                case (false, true):
+                    controller.Move(gamevelocity * Time.deltaTime * moving);
+                    break;
+                case (false, false):
+                    controller.Move(gamevelocity * Time.deltaTime * moving);
+                    break;
+            }
 
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
 
-        if (Input.GetAxis("Horizontal") != 0 & Input.GetAxis("Vertical") != 0)
-        {
-            gamevelocity = speed / Mathf.Sqrt(2);
+            if (Input.GetAxis("Horizontal") != 0 & Input.GetAxis("Vertical") != 0)
+            {
+                gamevelocity = speed / Mathf.Sqrt(2);
+            }
+            else
+            {
+                gamevelocity = speed;
+            }
         }
         else
         {
-            gamevelocity = speed;
+            x = Input.GetAxis("Horizontal");
+            z = Input.GetAxis("Vertical");
+
+            switch (Input.GetKey(KeyCode.LeftShift), Input.GetKey(KeyCode.LeftControl))
+            {
+                case (true, false):
+                    y = 1;
+                    break;
+                case (false, true):
+                    y = -1;
+                    break;
+                default:
+                    y = 0;
+                    break;
+            }
+            if (x == xChanched & Mathf.Abs(x) != 1 & x != 0)
+            {
+                x = 0;
+            }
+            else
+            {
+                if (Mathf.Abs(x) == 1)
+                {
+                    x = Input.GetAxisRaw("Horizontal");
+                }
+                else
+                {
+                    x = Input.GetAxis("Horizontal");
+                    xChanched = x;
+                }
+
+            }
+            if (z == zChanched & Mathf.Abs(z) != 1 & z != 0)
+            {
+                z = 0;
+            }
+            else
+            {
+                if (Mathf.Abs(z) == 1)
+                {
+                    z = Input.GetAxisRaw("Vertical");
+                }
+                else
+                {
+                    z = Input.GetAxis("Vertical");
+                    zChanched = z;
+                }
+            }
+            Vector3 moving = (transform.right * x + transform.forward * z + transform.up * y);
+            controller.Move(gamevelocity * Time.deltaTime * moving);
         }
 
+        if (Input.GetKey(KeyCode.LeftBracket) && Input.GetKey(KeyCode.E) && Input.GetKeyDown(KeyCode.Q))
+        {
+            if (!isGod)
+            {
+                isGod = true;
+            }
+            else
+            {
+                isGod = false;
+            }
+        }
     }
-    /*
-     x = Input.GetAxis("Horizontal");
-        z = Input.GetAxis("Vertical");
-
-        switch (Input.GetKey(KeyCode.LeftShift), Input.GetKey(KeyCode.LeftControl))
-        {
-            case (true, false):
-                y = 1;
-                break;
-            case (false, true):
-                y = -1;
-                break;
-            default:
-                y = 0;
-                break;
-        }
-        if (x == xChanched & Mathf.Abs(x) != 1 & x != 0)
-        {
-            x = 0;
-        }
-        else
-        {
-            if (Mathf.Abs(x) == 1)
-            {
-                x = Input.GetAxisRaw("Horizontal");
-            }
-            else
-            {
-                x = Input.GetAxis("Horizontal");
-                xChanched = x;
-            }
-
-        }
-        if (z == zChanched & Mathf.Abs(z) != 1 & z != 0)
-        {
-            z = 0;
-        }
-        else
-        {
-            if (Mathf.Abs(z) == 1)
-            {
-                z = Input.GetAxisRaw("Vertical");
-            }
-            else
-            {
-                z = Input.GetAxis("Vertical");
-                zChanched = z;
-            }
-        }
-        Vector3 moving = (transform.right * x + transform.forward * z + transform.up * y);
-        controller.Move(gamevelocity * Time.deltaTime * moving);
-    */
 }
